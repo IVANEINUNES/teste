14b42e5be1268f3cc043c9969d2cd1dc7521f654
IVANEINUNES <72806430+IVANEINUNES@users.noreply.github.com>
2020-10-14 21:09:10 -0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,16 +3,22 @@ let soma = 0
 let impar = 0
 input.onButtonPressed(Button.A, function () {
     par = randint(0, 5)
+    basic.showNumber(par)
+    basic.pause(500)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.AB, function () {
     soma = par + impar
     basic.showNumber(soma)
-    basic.pause(1000)
-    basic.clearScreen()
+    basic.pause(500)
     par = 0
     impar = 0
     soma = 0
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
     impar = randint(0, 5)
+    basic.showNumber(impar)
+    basic.pause(500)
+    basic.clearScreen()
 })
